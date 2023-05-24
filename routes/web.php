e9a43871e13ac111a9d54636f0523a7084b7dde2
fahminf <fnfadillah04@gmail.com>
2023-05-24 15:15:36 +0700

    Rental Mobil

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Faker\Guesser\Name;
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/welcome', function () {
+    echo "Selamat datang di Laravel";
+});
+
+Route::get('/greeting', function () {
+    return view('greeting');
+});
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/mobil', [MobilController::class, 'index']);
+Route::get('/mobil/create', [MobilController::class, 'create']); //memanggil form
+Route::post('/mobil/simpanData', [MobilController::class, 'store']); //mengirim request
+Route::get('/merk', [MerkController::class, 'index']);
