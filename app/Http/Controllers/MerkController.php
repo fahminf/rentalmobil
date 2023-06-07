cbd6cb576648f1a555aea63406e956cd82012520
fahminf <fnfadillah04@gmail.com>
2023-06-07 15:51:01 +0700

    praktikum 12

--- app/Http/Controllers/MerkController.php
+++ app/Http/Controllers/MerkController.php
@@ -26,4 +26,27 @@ class MerkController extends Controller
 
         return redirect()->to('/merk')->with('success', 'Data berhasil disimpan');
     }
+
+    function edit($id)
+    {
+        $merkData = Merk::find($id);
+        return view('pages.merk.edit', ['merkData' => $merkData]);
+    }
+
+    function update($id, Request $request)
+    {
+        $merkData = Merk::find($id);
+        $merkData->merk = $request->merk;
+        $merkData->save();
+
+        return redirect()->to('/merk')->with('succses', 'Data berhasil diupdate');
+    }
+
+    function delete($id)
+    {
+        $merkData = Merk::find($id);
+        $merkData->delete();
+
+        return redirect()->to('/merk')->with('succses', 'Data merk berhasil dihapus');
+    }
 }
